0544ae44c56777f40a6acff887ceae83be3d053d
electrone901 <Carbajalluis@rocketmail.com>
2025-01-21 18:45:48 -0500

    add home page

---
+++ src/app/customer/page.js
@@ -0,0 +1,8 @@
+export default function Customer() {
+  return (
+    <div className="container mx-auto p-6">
+      <h1 className="text-3xl font-bold mb-4">Customers</h1>
+      <p>View and manage your customers here.</p>
+    </div>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,66 @@
+'use client'
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            github commits: Here is going to
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const backgroundImageStyle = {
+    backgroundImage: 'url("cat-heart.png")',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    height: '100vh',
+  }
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const router = useRouter()
+  const poligonIdAuthentication = async () => {
+    console.log(petProfile)
+    // if (petProfile?.img === '') {
+    //   router.push('/selectCat')
+    // } else if (petProfile?.name === '') {
+    //   router.push('/nameCat')
+    // } else {
+    //   router.push('/statusCat')
+    // }
+  }
+  return (
+    <div className="p-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 gap-8 sm:gap-16">
+        <div className="mt-20 sm:mt-20 md:mt-24">
+          <div className="pt-24 text-[#C3994A] font-jetbrains-mono text-4xl font-extrabold leading-normal sm:text-xs">
+            <p className="">Turning every cat's</p>
+            <p>frown into a happy</p>
+            <p>purr</p>
+          </div>
+          {/* Buttons  */}
+          <button
+            className="bg-[#C3994A] text-white border-4 border-[#F5C364] hover:bg-[#F5C364]  rounded-lg w-[307px] mt-20  font-jetbrains-mono text-xl font-extrabold leading-normal p-4"
+            onClick={() => router.push('/statusCat')}
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            Upload Your Cat Story
+          </button>
+
+          <p className="text-black font-jetbrains-mono text-xs font-normal leading-normal mt-1">
+            Share cat stories to raise essential funds
+          </p>
+          <button
+            className="bg-[#C3994A] text-white border-4 border-[#F5C364] hover:bg-[#F5C364]  rounded-lg w-[307px] mt-20  font-jetbrains-mono text-xl font-extrabold leading-normal p-4"
+            onClick={() => router.push('/all-cats')}
           >
-            Read our docs
-          </a>
+            Find Your Cat Pal
+          </button>
+          <p className="text-black font-jetbrains-mono text-xs font-normal leading-normal mt-1">
+            Play games, help cats with donations
+          </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <div className="mt-12 sm:mt-96 md:mt-96">
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src="/cat-heart.png"
+            width={1900}
+            height={1900}
+            alt="pet"
+            className={`object-cover rounded-md w-full h-auto max-w-full`}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </div>
+      </div>
     </div>
   )
 }
