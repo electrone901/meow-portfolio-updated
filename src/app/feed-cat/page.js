683e867430b0d0d5c97f8e8fee71417775369c14
electrone901 <Carbajalluis@rocketmail.com>
2025-02-15 00:20:08 -0500

    fixing SelectFood page

--- src/app/feed-cat/page.js
+++ src/app/feed-cat/page.js
@@ -6,9 +6,19 @@ import { BsHeartFill } from 'react-icons/bs'
 import CatProfile from '@/src/components/CatProfile'
 
 function FeedCat() {
-  const [selected, setSelected] = useState(Number(-1))
+  const [selected, setSelected] = useState(-1)
   const [selectedFood, setSelectedFood] = useState('')
   const router = useRouter()
+
+  const foodItems = [
+    { name: 'Fish', image: '/image 1.png' },
+    { name: 'Grapes', image: '/image 2.png' },
+    { name: 'Fried Rice', image: '/image 3.png' },
+    { name: 'Coconut', image: '/image 4.png' },
+    { name: 'Watermelon', image: '/image 5.png' },
+    { name: 'Water', image: '/image 6.png' },
+  ]
+
   return (
     <div
       style={{
@@ -19,69 +29,67 @@ function FeedCat() {
       }}
     >
       <div className="flex items-center justify-center">
-        <div className="w-[90%] sm:w-[80%] md:w-[70%] mt-4 sm:mt-4 md:mt-40">
-          <div className="mb-2">
-            {selected == -1 && (
-              <p className="font-bold text-xl sm:text-2xl md:text-3xl text-black px-2 py-2 text-center">
-                Let's find out what your pet is in the mood to eat today
-              </p>
-            )}
-          </div>
+        <div className="w-[90%] sm:w-[80%] md:w-[70%]">
+          <p className="font-bold text-xl sm:text-2xl md:text-3xl text-black px-2 py-24 text-center">
+            Let's find out what your pet is in the mood to eat today
+          </p>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4">
-            <div className="col-span-1 sm:col-span-2 lg:col-span-2">
+          {/* GRID */}
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-8">
+            {/* ROW 1 */}
+            <div className="col-span-1 sm:col-span-2 lg:col-span-2 min-h-[300px] relative">
               {selected !== -1 && (
-                <div className="relative inline-block bg-white text-black p-4 rounded-lg items-center justify-center">
-                  <span> I love </span>
-                  <div className="relative z-1 flex items-center ">
-                    <p>{selectedFood} ~Meow~</p>
-                    <BsHeartFill color="red" fontSize={25} className="ml-1" />
-                    <BsHeartFill color="red" fontSize={25} className="ml-1" />
+                <div className="absolute top-[40px] right-0 bg-white text-black p-3 rounded-lg z-10 text-sm">
+                  <span> I love {selectedFood} </span>
+                  <div className="relative z-1 flex items-center">
+                    <p>~Meow~</p>
+                    <BsHeartFill color="red" fontSize={15} className="ml-1" />
+                    <BsHeartFill color="red" fontSize={15} className="ml-1" />
                   </div>
-                  <div className="absolute h-8 w-8 bg-white transform rotate-45 -mb-8 left-1/2 -translate-x-2 after:block after:w-8 after:h-8 after:bg-white after:absolute after:transform after:rotate-225 after:-mb-8 after:left-1/2 after:-translate-x-2"></div>
                 </div>
               )}
               <CatProfile />
             </div>
 
+            {/* ROW 2 */}
             <div className="col-span-1 sm:col-span-2 lg:col-span-3">
               <div className="bg-[#FFECCE] border-8 border-white rounded-md">
-                <div className="flex flex-col items-center justify-center pt-8">
+                <div className="flex flex-col items-center justify-center py-8">
                   <div className="flex items-center justify-around mb-4 flex-wrap">
-                    {['Coconut', 'Watermelon', 'Fish'].map((food, index) => (
+                    {foodItems.slice(0, 3).map((food, index) => (
                       <Image
                         key={index}
-                        src={`/image ${index + 1}.png`}
+                        src={food.image}
                         width={1900}
                         height={1900}
                         alt="pet"
                         className={`object-cover w-[100px] h-[100px] sm:w-[120px] sm:h-[120px] p-2 cursor-pointer ${
-                          selected === index + 1 &&
+                          selected === index &&
                           'border-4 border-dotted border-black p-4 rounded-lg'
                         }`}
                         onClick={() => {
-                          setSelected(index + 1)
-                          setSelectedFood(food)
+                          setSelected(index)
+                          setSelectedFood(food.name)
                         }}
                       />
                     ))}
                   </div>
 
                   <div className="flex items-center justify-around mt-4 flex-wrap">
-                    {['Grapes', 'Fried Rice', 'Water'].map((food, index) => (
+                    {foodItems.slice(3).map((food, index) => (
                       <Image
-                        key={food} // Use a unique key like food name instead of index
-                        src={`/image ${index + 4}.png`} // Correct the index for the images
+                        key={index + 3}
+                        src={food.image}
                         width={1900}
                         height={1900}
                         alt="pet"
                         className={`object-cover w-[100px] h-[100px] sm:w-[120px] sm:h-[120px] p-2 cursor-pointer ${
-                          selected === index + 4 &&
+                          selected === index + 3 &&
                           'border-4 border-dotted border-black p-4 rounded-lg'
                         }`}
                         onClick={() => {
-                          setSelected(index + 4)
-                          setSelectedFood(food)
+                          setSelected(index + 3)
+                          setSelectedFood(food.name)
                         }}
                       />
                     ))}
